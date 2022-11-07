8c492d3e5b73ac5a0fc549cbcf08d0ca399f42e1
omaralmgerbie <omar250abdo@gmail.com>
2022-11-07 13:32:23 +0200

    update example to new changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,31 +14,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   static const String _authority =
-      "https://msalfluttertest.b2clogin.com/tfp/3fab2993-1fec-4a8c-a6d8-2bfea01e64ea/B2C_1_phonesisu";
-  static const String _redirectUri = "msauth.com.muljin.msalflutterv2://auth";
-  static const String _clientId = "c3aab3bb-dd2e-4bb5-8768-38f032570a71";
+      "_authority";
+  static const String _iosRedirectUri = "_iosRedirectUri";
+  static const String _androidRedirectUri = "_androidRedirectUri";
+  static const String _clientId = "_clientId";
   String _output = 'NONE';
+  final config = MSALPublicClientApplicationConfig(
+    androidRedirectUri: _androidRedirectUri,
+    iosRedirectUri: _iosRedirectUri,
+    clientId: _clientId,androidConfig: MSALAndroidConfig(authorities:[Authority(authorityUrl: Uri.parse(_authority))] ),
+    authority: Uri.parse(_authority),
+  );
 
-  PublicClientApplication? pca;
+  MSALPublicClientApplication? pca;
+  List<MSALAccount>? accounts;
 
   Future<void> _acquireToken() async {
     print("called acquiretoken");
     //create the PCA if not already created
     if (pca == null) {
       print("creating pca...");
-      pca = await PublicClientApplication.createPublicClientApplication(
-          _clientId,
-          authority: _authority,
-          iosRedirectUri: _redirectUri);
+      pca = await MSALPublicClientApplication.createPublicClientApplication(
+          config);
+        await  pca!.initWebViewParams(MSALWebviewParameters());
     }
 
     print("pca created");
 
-    String res;
+    String res = '';
     try {
-      res = await pca!.acquireToken(
-          ["https://msalfluttertest.onmicrosoft.com/msaltesterapi/All"]);
-      print(res);
+      MSALResult? resp = await pca!.acquireToken(MSALInteractiveTokenParameters(
+          scopes: ['scopes']));
+      res = resp?.account.identifier?? 'noAuth';
     } on MsalUserCancelledException {
       res = "User cancelled";
     } on MsalNoAccountException {
@@ -55,19 +63,46 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> _loadAccount() async {
+    if (pca == null) {
+      print("initializing pca");
+      pca = await MSALPublicClientApplication.createPublicClientApplication(
+          config);
+        await  pca!.initWebViewParams(MSALWebviewParameters());
+
+    }
+    try {
+      final result = await pca!.loadAccounts();
+      if (result != null) {
+        accounts = result;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+     setState(() {
+    });
+  }
+
   Future<void> _acquireTokenSilently() async {
     if (pca == null) {
       print("initializing pca");
-      pca = await PublicClientApplication.createPublicClientApplication(
-          _clientId,
-          redirectUri: _redirectUri,
-          authority: _authority);
+      pca = await MSALPublicClientApplication.createPublicClientApplication(
+          config);
+        await  pca!.initWebViewParams(MSALWebviewParameters());
+
     }
 
-    String res;
+    String res = 'res';
     try {
-      res = await pca!.acquireTokenSilent(
-          ["https://msalfluttertest.onmicrosoft.com/msaltesterapi/All"]);
+      if (accounts?.isNotEmpty ==true) {
+        final response = await pca!.acquireTokenSilent(
+            MSALSilentTokenParameters(
+              scopes: ['scopes'],
+              // overrideAuthority: Authority(authorityUrl: Uri.parse(_authority)),
+            ),
+            accounts!.first);
+        res = response?.account.identifier ?? '';
+      }
     } on MsalUserCancelledException {
       res = "User cancelled";
     } on MsalNoAccountException {
@@ -91,15 +126,21 @@ class _MyAppState extends State<MyApp> {
   Future _logout() async {
     print("called logout");
     if (pca == null) {
-      pca = await PublicClientApplication.createPublicClientApplication(
-          _clientId,
-          authority: _authority);
+      print("initializing pca");
+      pca = await MSALPublicClientApplication.createPublicClientApplication(
+          config);
+        await  pca!.initWebViewParams(MSALWebviewParameters());
+
     }
 
     print("pca is not null");
     String res;
     try {
-      await pca!.logout();
+      if (accounts?.isNotEmpty == true) {
+        final resp =
+            await pca!.logout(MSALSignoutParameters(), accounts!.first);
+
+      }
       res = "Account removed";
     } on MsalException {
       res = "Error signing out";
@@ -127,11 +168,20 @@ class _MyAppState extends State<MyApp> {
                 onPressed: _acquireToken,
                 child: Text('AcquireToken()'),
               ),
+              
+              ElevatedButton(
+                  onPressed: _loadAccount,
+                  child: Text('loadAccount()')),
               ElevatedButton(
                   onPressed: _acquireTokenSilently,
                   child: Text('AcquireTokenSilently()')),
               ElevatedButton(onPressed: _logout, child: Text('Logout')),
               Text(_output),
+              Expanded(child: ListView.builder(itemCount: accounts?.length??0,
+                itemBuilder: (context, index) {
+                  final item = accounts![index];
+                return ListTile(title:Text(item.username??item.identifier) ,);
+              },))
             ],
           ),
         ),
